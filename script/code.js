7a9fb10199496e59f9d441e71069a6a6e507b5dd
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-24 14:09:01 +0200

    timeline

---
+++ script/code.js
@@ -0,0 +1,85 @@
+//about section
+let about = document.querySelector('section#about .row:last-child')
+let aboutContents = [
+    "I graduated high school at Alexander Sinton Highh School in 2020",
+    "My hobbies is to  play video games, pool and table tennis"
+]
+// Loop through the contents
+aboutContents.forEach( (content)=>{
+    about.innerHTML+=`
+    <p class='lead'>${content}</p>`
+} )
+//Education
+let education = [
+    {
+        id: 1,
+        year: 2020,
+        description: 'I got my certificates on Data science',
+        place: 'Data Camp',
+        type: 'certificate',
+        certificate: ''
+    },
+    {
+        id: 2,
+        year: 2019,
+        description: 'I got my certificates on Advanced to Python',
+        place: 'Data Camp',
+        type: 'certificate',
+        certificate: ''
+    },
+    {
+        id: 3,
+        year: 2018,
+        description: 'I got my certificates on Intro to Python',
+        place: 'Data Camp',
+        type: 'certificate',
+        certificate: ''
+    },
+    {
+        id: 4,
+        year: 2015,
+        description: 'I got my certificate on Intro to Android Developer',
+        place: 'Study Jams',
+        type: 'certificate',
+        certificate: ''
+    },
+    {
+        id: 5,
+        year: 2022,
+        description: 'I did a busines adminstrative course',
+        place: 'TSIBA College',
+        type: 'Certificate',
+        certificate: ''
+    },
+    {
+        id: 6,
+        year: 2021,
+        description: 'I did my course on I.T tecnical support',
+        place: 'icollege',
+        type: 'Certificate',
+        certificate: ''
+    },
+    {
+        id: 7,
+        year: 2020,
+        description: 'Grade 12',
+        place: '',
+        type: 'Certificate',
+        certificate: ''
+    },
+]
+//
+let divEducation = document.querySelector('.education');
+education.forEach((data)=> {
+    divEducation.innerHTML += `
+    <div class="card">
+        <h4 class="display-4">${data.year}</h4>
+        <div class="card-body">
+            <p class="text-white">
+                ${data.description} @ <span>${data.place}</span> 
+                <a href="${data.certificate}" target="_blank">${data.type}</a>
+            </p>
+        </div>
+    </div>
+    `
+})
